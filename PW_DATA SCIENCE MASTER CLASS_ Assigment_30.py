3d23dae5ff9ee9a4aa1e783da5d5bcc71975f57c
MANNAS RANJAN PANDEY <92135429+manas2829@users.noreply.github.com>
2023-03-28 23:35:31 +0530

    Add files via upload

---
+++ PW_DATA SCIENCE MASTER CLASS_ Assigment_30.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment_06.03.2023(Statistic)
+
+# ## Q1. What is Statistics?
+# 
+# ### Ans:-
+#         Statistics is the science of collecting,organizing and analyzing the data. It involves the use of various methods
+#         and techniques to gather and analyze numerical data, and then draw conclusions or make predictions based on that
+#         data.
+# 
+# 
+
+# ## Q2. Define the different type of statistics and given an example of when each type might be used?
+# 
+# ### Ans:-
+#             There are two main type of statistics
+#             
+#             1. Descriptive Statistics
+#             2. Infrenctial Statistics
+#             
+#         Descriptive Statistics :- It Consist of analyzing,summarizing,organizing data in the form of Numbers and graphs.
+#               Part of Descriptive Statistics ----
+#                   a) Measure of central tendancy (Ex- Mean,Median and Mode)
+#                   b) Measure of Dispersion - (Ex- Variance, Standard Deviation)
+#                   c) Correlation and Coveriance
+#                   d) Univarient and Bi-Varient analysis.
+#                   e) Visualization:- Bar Plots, Box Plots,Histogram,PDF,CDF,PMF, Normal Distibution.
+#                   
+#         Example :- A company collects data on the number of hours their employees work per week. Descriptive statistics 
+#                    would be used to calculate the mean, median, and standard deviation of the number of hours worked to 
+#                    describe the typical workweek of the employees.
+#                  
+#        Infrential Statistics:- Technique where we used the data that we have measured to form of conslusion.
+#                Part of Infrential Statistics------
+#                   a) Hypothesis Test.
+#                   b) Z-Test
+#                   c) T-Test
+#                   d) CHI Squere Test
+#                   e) F-Test(Anova Test)
+#                   
+#       Example :-A researcher wants to know if a new drug is effective in treating a particular medical condition. 
+#                 They would select a random sample of patients with the condition and give half the drug and the other 
+#                 half a placebo. Inferential statistics would be used to analyze the data from the two groups and draw 
+#                 conclusions about the effectiveness of the drug for the larger population of people with the condition.
+#                 
+
+# ## Q3. Categorise the following datasets with respect to quantative and qualitative data types
+# 
+# ### Ans:-
+#      i)  Grading in Exam :- A+,A,B+,B,C+,C,D,E -- QUALITATIVE 
+#      ii) Colour of Mangoes:- Yellow,Green,Orange,red - QUALITATIVE
+#      iii)Height data of a class : [178.9,179,179.5,176,177.2,178.3,178.8,...]--Quatative
+#      iv) Number of mangoes exported by farm : [500,600,478,672,...]---Quatative
+#      
+#      
+
+# ## Q4. Explain the concept of levels measurement and give an example of a variable for each level.
+# 
+# ### Ans:-
+# 
+#         Levels of measurement, also known as scales of measurement.
+#         There are four main levels of measurement: 1)nominal, 2)ordinal, 3)interval, and 4)ratio.
+#         
+#         1.Nominal level:- Nominal level data are categorical data that are used to classify or categorize objects or 
+#                           events. This level of measurement involves assigning numbers or labels to items without any
+#                           quantitative meaning.
+#           
+#           Example: Types of fruit such as apples, bananas, and oranges can be classified using nominal level data. 
+#                    Each fruit is assigned a label or number, but there is no inherent order or relationship between
+#                    them.
+#           
+#         2.Ordinal level:- Ordinal level data are used to rank or order items based on a particular characteristic or
+#                           attribute. This level of measurement involves assigning numbers to items in a way that reflects
+#                           their relative position or order.
+#                           
+#           Example: Ranking students in a class from highest to lowest based on their grade point average is an example of 
+#                    ordinal level data. The ranking is based on the order of the students' GPAs, but the difference between
+#                    each ranking may not be equal.
+#                    
+#        3.Interval level:- Interval level data involves the use of numerical data with equal intervals between each value. 
+#                           This level of measurement involves assigning numbers to items in a way that reflects their 
+#                           relative magnitude and the difference between each value is meaningful.
+#                           
+#           Example: Measuring temperature in Celsius or Fahrenheit is an example of interval level data. The difference
+#                    between 10 and 20 degrees Celsius is the same as the difference between 20 and 30 degrees Celsius.
+#              
+#        4.Ratio level: Ratio level data involves the use of numerical data with a true zero point. This level of measurement
+#                       involves assigning numbers to items in a way that reflects their relative magnitude and the ratio 
+#                       between each value is meaningful.
+#                       
+#          Example: Measuring weight or height is an example of ratio level data. A person's weight of 60 kilograms is twice 
+#                   as much as a person who weighs 30 kilograms. Similarly, a person who is 180 centimeters tall is twice as
+#                   tall as a person who is 90 centimeters tall.
+#                   
+#                    
+#       
+# 
+# 
+
+# ## Q6. Why is it important to understand the level of measurement when analyzing data?provide an example to illustrate your answer.
+# 
+# ### Ans:-
+# 
+#             It is important to understand the level of measurement when analyzing data because it determines the type of
+#             statistical analyses that can be used and the appropriate interpretations that can be made from the data. 
+#             Different statistical tests and analyses are designed to work with different types of data, and using the wrong
+#             test can lead to incorrect conclusions and misleading results.
+#             
+#             For example, consider a study that measures the heights of students in a classroom. If the heights are measured
+#             using a nominal scale, which simply categorizes individuals into groups without any numerical value, such as 
+#             "short" or "tall," then it would not make sense to calculate the mean height or perform other statistical 
+#             analyses that rely on numerical values. In this case, the best analysis would be to simply report the number
+#             of students in each height category.
+#             
+#             On the other hand, if the heights are measured using a continuous scale, such as inches or centimeters, then it
+#             would be appropriate to use statistical analyses that can handle continuous data, such as calculating the mean
+#             or standard deviation. Using a nominal scale for continuous data would be inappropriate and would not provide 
+#             meaningful results.
+#             
+# 
+# 
+
+# ## Q7. How Nominal data type is different from Ordinal data type.
+# 
+# ### Ans:-
+# 
+#     Nominal data refers to data that can be categorized into distinct categories or groups, without any inherent order or
+#     ranking. Examples of nominal data include gender (male/female), race (Asian, Black, Hispanic, White), and favorite color
+#     (red, blue, green). Nominal data can be counted and percentages can be calculated, but no meaningful mathematical 
+#     operations, such as addition or multiplication, can be performed on nominal data.
+#     
+#     Ordinal data, on the other hand, is a type of data that can be ranked or ordered based on some criteria or attribute, 
+#     but the intervals between categories are not necessarily equal. Examples of ordinal data include rank orders (first,
+#     second, third), education level (high school, college, graduate school), and rating scales (poor, fair,good,excellent).
+#     Ordinal data can be counted and ranked, and some mathematical operations such as median, mode, and percentiles can be
+#     calculated, but not mean or standard deviation.
+#        
+
+# ## Q8. Which type of Plot cab be used to display data in terms of range?
+# 
+# ### Ans:- 
+#      A box plot (also known as a box-and-whisker plot) is commonly used to display data in terms of range.
+
+# ## Q9. Describe the different between descriptive and inferential statistics.Give an example of each type of statistics and example how they are used.
+# 
+# ### Ans:-
+# 
+#     Descriptive statistics refer to methods used to summarize and describe the important features of a dataset, such as its
+#     mean, median, mode, standard deviation, range, and frequency distribution. Descriptive statistics are used to provide a 
+#     concise and meaningful summary of a dataset, and can be used to identify patterns, trends, and outliers in the data.
+# 
+#     An example of descriptive statistics is the calculation of the mean and standard deviation of a set of exam scores. 
+#     These statistics can be used to describe the overall performance of the students in the exam,and to identify whether 
+#     the scores are evenly distributed or skewed towards higher or lower values.
+#     
+#     Inferential statistics, on the other hand, refer to methods used to make inferences or predictions about a larger 
+#     population based on a sample of data. Inferential statistics involve testing hypotheses, estimating population 
+#     parameters, and calculating probabilities.
+# 
+#     An example of inferential statistics is a hypothesis test to determine whether a new drug is effective in treating a 
+#     certain condition. A sample of patients is randomly assigned to receive the new drug or a placebo, and their outcomes
+#     are compared. Based on the results of the study, researchers can infer whether the new drug is more effective than 
+#     the placebo for the larger population of patients with the same condition.
+# 
+
+# ## Q.10. What are some common measures of Central tendency and variability used in statistics? Explain how each measure can be used to describe a dataset.
+# 
+# ### Ans:-
+#      Measures of central tendency refer to the typical or average value in a dataset. The most commonly used measures of 
+#      central tendency are:
+#      
+#      1.Mean: The mean is the sum of all values in the dataset divided by the number of values. The mean is a useful measure
+#              of central tendency when the data is normally distributed and there are no extreme values (outliers) in the
+#              dataset.
+#      2.Median: The median is the middle value in a dataset when the data is arranged in order of magnitude. The median is a
+#                useful measure of central tendency when the data is skewed or has outliers, as it is not affected by extreme
+#                values.
+#      3.Mode: The mode is the most frequently occurring value in a dataset. The mode is a useful measure of central tendency
+#              when the data is categorical or discrete.
+#              
+#      Measures of variability, on the other hand, describe how spread out or dispersed the values in a dataset are. The most
+#      commonly used measures of variability are:  
+#      
+#      1.Range: The range is the difference between the maximum and minimum values in a dataset. The range is a useful measure 
+#               of variability when the data is evenly distributed.
+#               
+#      2.Variance: The variance is the average of the squared differences from the mean. The variance measures the variability
+#                  of the dataset by comparing each data point to the mean, and then squaring and averaging the differences.
+#                  
+#      3.Standard deviation: The standard deviation is the square root of the variance. The standard deviation is a useful 
+#                            measure of variability because it is expressed in the same units as the data, and it gives an
+#                            indication of how tightly the data is clustered around the mean.
